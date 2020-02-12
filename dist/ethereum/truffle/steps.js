758175efbc41611c72a23bb919bc143b13047e22
Claudia Bartoli Duncan <claudia@stampery.co>
2020-02-12 15:33:37 +0100

    fix: add changes in dist/
    Signed-off-by: Claudia Bartoli Duncan <claudia@stampery.co>

--- dist/ethereum/truffle/steps.js
+++ dist/ethereum/truffle/steps.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -33,13 +35,15 @@ var _protocolBuffers = _interopRequireDefault(require("protocol-buffers"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
@@ -53,7 +57,7 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -188,12 +192,12 @@ function writeMigrations(contractNames, userContractsDir, migrationsDir, fs) {
   });
   var stage2 = "// WARNING: DO NOT DELETE THIS FILE\n// This file was auto-generated by the Witnet compiler, any manual changes will be overwritten.\nconst WitnetBridgeInterface = artifacts.require(\"WitnetBridgeInterface\")\nconst MockWitnetBridgeInterface = artifacts.require(\"MockWitnetBridgeInterface\")\nconst CBOR = artifacts.require(\"CBOR\")\nconst Witnet = artifacts.require(\"Witnet\")\n\nconst addresses = ".concat(JSON.stringify(Addresses, null, 2).replace(/(["}])$\n/gm, function (m, p1) {
     return "".concat(p1, ",\n");
-  }), "\n\nmodule.exports = function (deployer, network) {\n  network = network.split(\"-\")[0]\n  if (network in addresses) {\n    Witnet.address = addresses[network][\"Witnet\"]\n    WitnetBridgeInterface.address = addresses[network][\"WitnetBridgeInterface\"]\n  } else {\n    deployer.deploy(CBOR)\n    deployer.link(CBOR, Witnet)\n    deployer.deploy(Witnet)\n    deployer.deploy(MockWitnetBridgeInterface).then(function() {\n    return  WitnetBridgeInterface.address = MockWitnetBridgeInterface.address\n  })\n  }\n}\n");
+  }), "\n\nmodule.exports = function (deployer, network) {\n  network = network.split(\"-\")[0]\n  if (network in addresses) {\n    Witnet.address = addresses[network][\"Witnet\"]\n    WitnetBridgeInterface.address = addresses[network][\"WitnetBridgeInterface\"]\n  } else {\n    deployer.deploy(CBOR)\n    deployer.link(CBOR, Witnet)\n    deployer.deploy(Witnet)\n    deployer.deploy(MockWitnetBridgeInterface).then(function() {\n     return WitnetBridgeInterface.address = MockWitnetBridgeInterface.address\n    })\n  }\n}\n");
   fs.writeFileSync("".concat(migrationsDir, "2_witnet_core.js"), stage2);
   var userContractsArgs = readMigrationArgs(migrationsDir, fs);
   var stage3 = "// This file was auto-generated by the Witnet compiler, any manual changes will be overwritten except\n// each contracts' constructor arguments (you can freely edit those and the compiler will respect them).\nconst Witnet = artifacts.require(\"Witnet\")\nconst WitnetBridgeInterface = artifacts.require(\"WitnetBridgeInterface\")\n".concat(artifacts.map(function (artifact) {
     return "const ".concat(artifact, " = artifacts.require(\"").concat(artifact, "\")");
-  }).join("\n"), "\n\nmodule.exports = function (deployer) {\n  deployer.link(Witnet, [").concat(artifacts.join(", "), "])\n").concat(artifacts.map(function (artifact) {
+  }).join("\n"), "\nmodule.exports = function (deployer) {\n  deployer.link(Witnet, [").concat(artifacts.join(", "), "])\n").concat(artifacts.map(function (artifact) {
     if (artifact in userContractsArgs) {
       var args = userContractsArgs[artifact].split(/[(,)]/).slice(2).reverse().slice(1).reverse().map(function (x) {
         return x.trim();
--- dist/witnet/stages.js
+++ dist/witnet/stages.js
@@ -11,7 +11,9 @@ var _script = require("../radon/script");
 
 var _types = require("../radon/types");
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _toArray(arr) { return _arrayWithHoles(arr) || _iterableToArray(arr) || _nonIterableRest(); }
 
@@ -25,7 +27,7 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
