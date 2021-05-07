c94ee0dc1ebc2ca84e8fecb3968d70731c770f04
Adán SDPC <adan.sdpc@gmail.com>
2021-05-07 12:20:25 +0200

    fix: use latest version of the request board contracts
    WitnetRequestsBoardProxy → WitnetRequestBoardProxy
    MockWitnetRequestsBoard → WitnetRequestBoard

--- dist/ethereum/addresses.js
+++ dist/ethereum/addresses.js
@@ -8,13 +8,13 @@ var goerli = {
   BlockRelayProxy: "0xC26a3E374b3bC1a58F9da87e4C1e82eF211074E8",
   CBOR: "0x756b25B92c6F3406C3B4dD2576F8E582E0Fedc43",
   Witnet: "0x094D115bcD3C41b95285B8CaAa41AFe5122F4bC3",
-  WitnetRequestsBoardProxy: "0x2c4f2274bbd6088A62f59E899b6d2fdcB5551574"
+  WitnetRequestBoardProxy: "0x2c4f2274bbd6088A62f59E899b6d2fdcB5551574"
 };
 exports.goerli = goerli;
 var rinkeby = {
   BlockRelayProxy: "0x44b62346B14Dc974aB7D998782458cDE10B846B1",
   CBOR: "0x53E07a97D177B97BDB93F43FF37BB517cC48Ed5c",
   Witnet: "0xCc9029b77bb0380EF204A8E55C7c8B81982202CB",
-  WitnetRequestsBoardProxy: "0x3f2c3b78bc4772feE0f1fD9c575Ed5A72774878c"
+  WitnetRequestBoardProxy: "0x3f2c3b78bc4772feE0f1fD9c575Ed5A72774878c"
 };
 exports.rinkeby = rinkeby;
