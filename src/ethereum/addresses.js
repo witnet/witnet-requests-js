a528e998ab96c3a6c999144b5e773894aad33575
Guillermo Díaz <guillermo@otherplane.com>
2021-06-24 17:11:52 +0200

    fix(conflux.test): add deployed addresses

--- src/ethereum/addresses.js
+++ src/ethereum/addresses.js
@@ -11,8 +11,8 @@ const rinkeby = {
 };
 
 const conflux = {
-  "Witnet": "",
-  "WitnetProxy": ""
+  "Witnet": "0x8239dbA17E9E922c62128d4083B8627b3b63f69a",
+  "WitnetProxy": "0x851733Ec551720fF2F6e249114435CD00C5585Da"
 };
 
 export {
