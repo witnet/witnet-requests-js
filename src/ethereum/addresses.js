c3966fcf3550cea545fc36c9ccca26aba8cdb58e
Guillermo Díaz <guillermo@otherplane.com>
2021-07-06 14:19:22 +0200

    feat: add kovan addresses

--- src/ethereum/addresses.js
+++ src/ethereum/addresses.js
@@ -9,9 +9,15 @@ const rinkeby = {
   "Witnet": "0x5259aCEfF613b37aF35999798A6da60bEF326038",
   "WitnetRequestBoardProxy": "0x9b42b0D80C428B17A5828dF5C2c96454ca54bD04"
 };
+
+const kovan = {
+  "CBOR": "0xB4B2E2e00e9d6E5490d55623E4F403EC84c6D33f",
+  "Witnet": "0xD9465D38f50f364b3263Cb219e58d4dB2D584530",
+  "WitnetRequestBoardProxy": "0xD9a6d1Ea0d0f4795985725C7Bd40C31a667c033d"
 };
 
 export {
   goerli,
-  rinkeby
+  rinkeby,
+  kovan
 }
